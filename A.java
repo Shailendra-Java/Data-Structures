ff995ef438fd474c9d17d1c40c1405d8c5befe1e
nazirmohd2006 <nazirmohd2006@gmail.com>
2018-06-05 16:03:25 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,7 +1,12 @@
 class A
 {
+  public A()
+  {
+     System.out.println("A is instantiated.");
+  }
  public static void main(String arr[])
  {
    System.out.println("Check this!");
+   A obj = new A();
   }
 }
