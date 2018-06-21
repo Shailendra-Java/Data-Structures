88ed64bab3404cdd4e621c447163df975b35c8d1
Shailendra Singh <20810852+Shailendra-Java@users.noreply.github.com>
2018-06-21 15:40:09 +0530

    updates in selection sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-	int size, arr[50], i, j, temp;
+	int size, arr[50], i, j, temp, min_index;
 	cout<<"Enter Array Size : ";
 	cin>>size;
 	cout<<"Enter Array Elements : ";
@@ -13,15 +14,17 @@ int main()
 	cout<<"Sorting array using selection sort...\n";
 	for(i=0; i<size; i++)
 	{
+		min_index = i;
 		for(j=i+1; j<size; j++)
 		{
 			if(arr[i]>arr[j])
 			{
-				temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
+				min_index = j;
 			}
 		}
+		temp = arr[min_index];
+		arr[min_index] = arr[i];
+		arr[i] = temp;
 	}
 	cout<<"Now the Array after sorting is :\n";
 	for(i=0; i<size; i++)
