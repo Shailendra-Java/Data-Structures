85697e7b6166c0db4e7b352bae54b5e36d4cbb7a
Shailendra Singh <20810852+Shailendra-Java@users.noreply.github.com>
2019-04-06 10:46:21 +0530

    List Updated

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,282 +1,250 @@
 #include<iostream>
-#include<stdlib.h>
 using namespace std;
 
 class Node
 {
-	public:
-		int data;
-		Node *next;
-		Node *prev;
+    public:
+        int data;
+        Node *next;
+        Node *prev;
 };
-class DoublyListDemo
-{
-	public:
-		Node *start;
-		DoublyListDemo()
-		{
-			start = NULL;
-		}
 
-		Node* createNode(int x)
-		{
-			Node *temp = new Node;
-			if(temp==NULL)
-				cout<<"Node creation failed"<<endl;
-			else
-			{
-				temp->data = x;
-				temp->next = NULL;
-				temp->prev = NULL;
-			}
-			return temp;
-		}
-		void insertFront()
-		{
-            int data;
-            Node *temp;
-            cout<<"Enter any element"<<endl;
-            cin>>data;
-            temp = createNode(data);
-            if(start==NULL)
+class Operations
+{
+    Node *START, *LAST;
+    public:
+        Operations()
+        {
+            START = LAST = NULL;
+        }
+        Node* createNode(int n)
+        {
+            Node *temp = new Node;
+            temp->data = n;
+            temp->next = temp->prev = NULL;
+            return temp;
+        }
+        void insertAtFront()
+        {
+            int num;
+            Node *newNode;
+            cout<<"Enter a number"<<endl;
+            cin>>num;
+            newNode = createNode(num);
+            if(START == NULL)
             {
-                start = temp;
-                cout<<"First Node created"<<endl;
+                START = LAST = newNode;
+                cout<<"First Node inserted"<<endl;
             }
             else{
-                temp->next = start;
-                temp->prev = NULL;
-                start = temp;
-                cout<<"Node inserted "<<endl;
+                START->prev = newNode;
+                START = newNode;
+                cout<<"Node inserted at Front"<<endl;
             }
-
-		}
-		void insertAt()
-		{
-			Node *current, *prev, *temp;
-			int data;
-			cout<<"Enter Element"<<endl;
-			cin>>data;
-			temp = createNode(data);
-			current = start;
-			prev = NULL;
-			if(start==NULL)
-			{
-				start = temp;
-				cout<<"First Node created"<<endl;
-			}
-			else
-            {
-				while(current->data < temp->data && current != NULL)
-				{
-				    prev = current;
-					current = current->next;
-				}
-				temp->next = current;
-				temp->prev = prev;
-				if(current != NULL)
-                    current->prev = temp;
-				prev->next = temp;
-				cout<<"Node inserted at their correct position"<<endl;
-            }
-		}
-		void insertLast()
-		{
-			Node *temp, *last;
-			int num;
-			cout<<"Enter a number"<<endl;
-			cin>>num;
-			temp = createNode(num);
-			last = start;
-			if(start==NULL)
-			{
-				start = temp;
-				cout<<"First Node created"<<endl;
-			}
-			else
-			{
-				while(last->next != NULL)
-				{
-					last = last->next;
-				}
-				last->next = temp;
-				temp->prev = last;
-				cout<<"Node inserted"<<endl;
-			}
-		}
-
-        void deleteFront()
-		{
-            Node *current;
-            current = start;
-            if(start == NULL)
-                cout<<"Sorry List is empty"<<endl;
-            else
-            {
-                start = start->next;
-                start->prev = NULL;
-                cout<<"Node to be deleted :  "<<current->data<<endl;
-                delete(current);
-                cout<<"First Node Deleted"<<endl;
-            }
-		}
-
-		void deleteMid()
-		{
-            Node *prev, *current;
-            int data;
-            cout<<"Enter element to delete"<<endl;
-            cin>>data;
-            prev = current = start;
-            if(start==NULL)
-            {
-                cout<<"Sorry list is empty"<<endl;
-            }
-            else
-            {
-                while(current->data != data && current->next != NULL)
-                {
-                    prev = current;
-                    current = current->next;
-                }
-                prev->next = current->next;
-                if(current->next != NULL)
-                    current->next->prev = prev;
-                cout<<"Node to be deleted :  "<<current->data<<endl;
-                delete(current);
-                cout<<"Node deleted"<<endl;
-            }
-		}
-
-		void deleteLast()
-		{
-            Node *last, *prev;
-            last = start;
+        }
+        void insertAtMid()
+        {
+            int num;
+            Node *newNode, *curr, *prev;
+            cout<<"Enter a number"<<endl;
+            cin>>num;
+            newNode = createNode(num);
+            curr = START;
             prev = NULL;
-            if(start == NULL)
-                cout<<"List is empty"<<endl;
-            else
+            if(START == NULL)
             {
-                while(last->next != NULL){
-                    prev = last;
-                    last = last->next;
-                }
-                prev->next = NULL;
-                cout<<"Node to be deleted :  "<<last->data<<endl;
-                delete(last);
-                cout<<"Node deleted"<<endl;
+                START = LAST = newNode;
+                cout<<"First Node inserted"<<endl;
             }
-		}
-
-		void displayForwad()
-		{
-			Node *temp;
-			temp = start;
-			if(start==NULL)
-				cout<<"List is empty"<<endl;
-			else
-			{
-			    cout<<"null <- ";
-				while(temp != NULL)
+            else{
+                while(num > curr->data && curr->next != NULL)
                 {
-                    if(temp->next == NULL)
-                    {
-                        cout<<temp->data<<" -> ";
-                        temp = temp->next;
-                    }
-                    else
-                    {
-                        cout<<temp->data<<" <-> ";
-                        temp = temp->next;
-                    }
-				}
-				cout<<"null"<<endl;
-			}
-		}
-		void displayBackwad()
-		{
-			Node *last;
-			last = start;
-			if(start==NULL)
-				cout<<"List is empty"<<endl;
-			else
-			{
-			    while(last->next != NULL)
-				{
-					last = last->next;
-				}
-				cout<<"null <- ";
-				while(last != NULL)
+                    prev = curr;
+                    curr = curr->next;
+                }
+                if(curr->next == NULL)
                 {
-                    if(last->prev == NULL)
-                    {
-                        cout<<last->data<<" -> ";
-                        last = last->prev;
-                    }
-                    else
-                    {
-                        cout<<last->data<<" <-> ";
-                        last = last->prev;
-                    }
-				}
-				cout<<"null"<<endl;
-			}
-		}
+                    curr->next = newNode;
+                    newNode->prev = curr;
+                    LAST = newNode;
+                }
+                else if(prev == NULL)
+                {
+                    curr->prev = newNode;
+                    newNode->next = curr;
+                    START = newNode;
+                }
+                else{
+                    prev->next = newNode;
+                    newNode->prev = prev;
+                    newNode->next = curr;
+                    curr->prev = newNode;
+                }
+                cout<<"Node inserted at their position"<<endl;
+            }
+        }
+        void insertAtLast()
+        {
+            int num;
+            Node *newNode;
+            cout<<"Enter a number"<<endl;
+            cin>>num;
+            newNode = createNode(num);
+            if(START == NULL)
+            {
+                START = LAST = newNode;
+                cout<<"First Node inserted"<<endl;
+            }
+            else{
+                LAST->next = newNode;
+                newNode->prev = LAST;
+                LAST = newNode;
+                cout<<"Node inserted at Last"<<endl;
+            }
+        }
+        void deleteFromFront()
+        {
+            Node *curr = START;
+            if(START == NULL)
+            {
+                cout<<"List is empty"<<endl;
+            }
+            else{
+                START = START->next;
+                START->prev = NULL;
+                delete(curr);
+                cout<<"Node Deleted from front"<<endl;
+            }
+        }
+        void deleteFromMid()
+        {
+            int num;
+            Node *curr, *prev;
+            cout<<"Enter a number to delete"<<endl;
+            cin>>num;
+            curr = START;
+            prev = NULL;
+            if(START == NULL)
+            {
+                cout<<"List is empty"<<endl;
+            }
+            else{
+                while(num != curr->data && curr->next != NULL)
+                {
+                    prev = curr;
+                    curr = curr->next;
+                }
+                if(START == LAST)
+                {
+                    START = LAST = NULL;
+                    delete(curr);
+                }
+                else if(curr->next == NULL)
+                {
+                    LAST = LAST->prev;
+                    LAST->next = NULL;
+                    delete(curr);
+                }
+                else if(prev == NULL)
+                {
+                    START = START->next;
+                    START->prev = NULL;
+                    delete(curr);
+                }
+                else{
+                    prev->next = curr->next;
+                    curr->next->prev = prev;
+                    delete(curr);
+                }
+                cout<<"Node deleted from their position"<<endl;
+            }
+        }
+        void deleteFromLast()
+        {
+            Node *curr = LAST;
+            if(START == NULL)
+            {
+                cout<<"List is empty"<<endl;
+            }
+            else{
+                LAST = LAST->prev;
+                LAST->next = NULL;
+                delete(curr);
+                cout<<"Node Deleted from last"<<endl;
+            }
+        }
+        void printAsc()
+        {
+            Node *curr = START;
+            if(START == NULL)
+                cout<<"List is empty"<<endl;
+            else{
+                while(curr != NULL)
+                {
+                    cout<<curr->data<<"  ";
+                    curr = curr->next;
+                }
+                cout<<endl;
+            }
+        }
+        void printDesc()
+        {
+            Node *curr = LAST;
+            if(START == NULL)
+                cout<<"List is empty"<<endl;
+            else{
+                while(curr != NULL)
+                {
+                    cout<<curr->data<<"  ";
+                    curr = curr->prev;
+                }
+                cout<<endl;
+            }
+        }
 };
-int main()
+main()
 {
-	DoublyListDemo dld;
-	char choice;
-	int opr;
-	do
+    Operations lio;
+    int choice;
+    do
     {
-        cout<<"Linked List Operations"<<endl;
-        cout<<"============================="<<endl;
-        cout<<"1=> Insert at beginning"<<endl;
-        cout<<"2=> Insert at position"<<endl;
-        cout<<"3=> Insert at last"<<endl;
-        cout<<"4=> Delete at beginning"<<endl;
-        cout<<"5=> Delete at position"<<endl;
-        cout<<"6=> Delete at last"<<endl;
-        cout<<"7=> Display in forward direction"<<endl;
-        cout<<"8=> Display in backward direction"<<endl;
-        cout<<"9=> Exit"<<endl;
-        cout<<"------------------------------"<<endl;
-        cout<<"Enter your choice"<<endl;
-        cin>>opr;
-        switch(opr)
+        cout<<"1. Insert Front\t2. Insert Mid"<<endl;
+        cout<<"3. Insert Last\t4. Display"<<endl;
+        cout<<"5. Delete Front\t6. Delete Mid"<<endl;
+        cout<<"7. Delete Last\t8. Search"<<endl;
+        cout<<"0. Exit"<<endl;
+        cin>>choice;
+        switch(choice)
         {
             case 1:
-                dld.insertFront();
+                lio.insertAtFront();
                 break;
             case 2:
-                dld.insertAt();
+                lio.insertAtMid();
                 break;
             case 3:
-                dld.insertLast();
+                lio.insertAtLast();
                 break;
             case 4:
-                dld.deleteFront();
+                cout<<"Forward Direction >>"<<endl;
+                lio.printAsc();
+                cout<<"Backward Direction >>"<<endl;
+                lio.printDesc();
                 break;
             case 5:
-                dld.deleteMid();
+                lio.deleteFromFront();
                 break;
             case 6:
-                dld.deleteLast();
+                lio.deleteFromMid();
                 break;
             case 7:
-                dld.displayForwad();
+                lio.deleteFromLast();
                 break;
-            case 8:
-                dld.displayBackwad();
-                break;
-            case 9:
-                exit(0);
-            default:
-                cout<<"Invalid Operation"<<endl;
+            /*case 8:
+                lio.search();
+                break; */
+
         }
-        cout<<"Do you want to continue (y/n) "<<endl;
-        cin>>choice;
-    }while(choice!= 'n');
-	return 0;
+    }while(choice != 0);
 }
+
